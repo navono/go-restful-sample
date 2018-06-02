de239c3abe2c54df37f8333aab8350557ed53aaa
pingqixing <navono007@gmail.com>
2018-06-02 11:38:44 +0800

    链式 middleware

--- middleware/cityAPI.go
+++ middleware/cityAPI.go
@@ -3,12 +3,45 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type city struct {
 	Name string
 	Area uint64
+}
+
+// Middleware to check content type as JOSN
+func filterContenType(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Currently in the check content type middleware")
+
+		// Filtering requests by MIME type
+		if r.Header.Get("Content-type") != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte("415 - Unsupported Media Type. Please send JSON"))
+			return
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
+
+// Middleware to add server timestamp for response cookie
+func setServerTimeCookie(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+
+		// Setting cookie to each and every response
+		cookie := http.Cookie{
+			Name:  "Server-Time(UTC)",
+			Value: strconv.FormatInt(time.Now().Unix(), 10),
+		}
+		http.SetCookie(w, &cookie)
+		log.Println("Currently in the set server time middleware")
+	})
 }
 
 func mainLogic(w http.ResponseWriter, r *http.Request) {
@@ -28,15 +61,16 @@ func mainLogic(w http.ResponseWriter, r *http.Request) {
 
 		// Everything is fine
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("201 - Created"))
+		w.Write([]byte("201 - Created\n"))
 	} else {
 		// Method not allowed
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("405 - Method Not Allowed"))
+		w.Write([]byte("405 - Method Not Allowed\n"))
 	}
 }
 
 func main() {
-	http.HandleFunc("/city", mainLogic)
+	mainLogicHandler := http.HandlerFunc(mainLogic)
+	http.Handle("/city", filterContenType(setServerTimeCookie(mainLogicHandler)))
 	http.ListenAndServe(":8000", nil)
 }
